2c65d2362134161246591625b1cb674c6ed53d63
Savannah Mercedes <savannahmunn1996@gmail.com>
2022-12-01 22:10:24 -0800

    adding bulma css design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 var genre = [];
 var genre_imdbAPI =
-    'https://imdb-api.com/API/AdvancedSearch/k_gqv62f21/?genres='; //comma separated values
+  "https://imdb-api.com/API/AdvancedSearch/k_gqv62f21/?genres="; //comma separated values
 var length_imdbAPI =
-    'https://imdb-api.com/API/AdvancedSearch/k_gqv62f21?moviemeter='; //comma separated numbers
+  "https://imdb-api.com/API/AdvancedSearch/k_gqv62f21?moviemeter="; //comma separated numbers
 var actor;
-var actor_imdbAPI = 'https://imdb-api.com/en/API/SearchName/k_gqv62f21/'; //requires an actors name
-var name_imdbAPI = 'https://imdb-api.com/API/Name/k_gqv62f21/';
+var actor_imdbAPI = "https://imdb-api.com/en/API/SearchName/k_gqv62f21/"; //requires an actors name
+var name_imdbAPI = "https://imdb-api.com/API/Name/k_gqv62f21/";
 var imdbID;
 var watchAPI =
-    'https://api.watchmode.com/v1/' +
-    imdbID +
-    '/345534/sources/?apiKey=VdPS48VbVT9u5JoyVOSjCyMC8zbheghplfqA9HX9/'; //title must be defined, should used imdbID.
-var omdbAPI = 'http://www.omdbapi.com/?i=tt3896198&apikey=c4ce22ab'; //might not even be used
-var ratingsAPI = 'https://imdb-api.com/en/API/Ratings/k_gqv62f21/'; //requires imdbID
-
+  "https://api.watchmode.com/v1/" +
+  imdbID +
+  "/345534/sources/?apiKey=VdPS48VbVT9u5JoyVOSjCyMC8zbheghplfqA9HX9/"; //title must be defined, should used imdbID.
+var omdbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=c4ce22ab"; //might not even be used
+var ratingsAPI = "https://imdb-api.com/en/API/Ratings/k_gqv62f21/"; //requires imdbID
