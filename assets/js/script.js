80ccf683e151aaf5d7eea4396d89fa7cff389966
mkalik <mbadiankouyate@gmail.com>
2022-12-03 23:52:36 -0800

    working on card layouts for an actor search-just figuring out functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var genre_imdbAPI =
     'https://imdb-api.com/API/AdvancedSearch/k_gqv62f21/?genres=';
 var length_imdbAPI =
-  "https://imdb-api.com/API/AdvancedSearch/k_gqv62f21?moviemeter="; //comma separated numbers
+    'https://imdb-api.com/API/AdvancedSearch/k_gqv62f21?moviemeter='; //comma separated numbers
 var actor;
 var actor_imdbAPI = 'https://imdb-api.com/en/API/SearchName/k_gqv62f21/'; //requires an actors name
 var name_imdbAPI = 'https://imdb-api.com/API/Name/k_gqv62f21/'; //requires imdbID
@@ -16,6 +16,7 @@ var ratingsAPI = 'https://imdb-api.com/en/API/Ratings/k_gqv62f21/'; //requires i
 
 var id_title = {};
 var json_actor;
+var actorInfo = document.querySelector('.actor-info');
 
 actor = prompt('enter an actors name', '');
 getActorID(actor);
@@ -23,13 +24,12 @@ var genre;
 // genre = prompt('enter a genre', '');
 // getGenre(genre);
 function getGenre(type) {
-    fetch(genre_imdbAPI + type)
-        .then(function (data) {
-            return data.json();
-        })
-        .then(function (results) {
-            console.log(results);
-        });
+    fetch(genre_imdbAPI + type).then(function (data) {
+        return data.json();
+    });
+    // .then(function (results) {
+    //     console.log(results);
+    // });
 }
 
 function getActorID(name) {
@@ -50,28 +50,41 @@ function knownFor(name) {
             return name_info.json();
         })
         .then(function (info) {
-            console.log('in knownfor:' + info);
-            var movies = info.knownFor;
-            var known = {};
-            for (var i = 0; i < movies.length; i++) {
-                var id = movies[i].id;
-                var title = movies[i].title;
-                known[id] = title;
-            }
-            useData(known);
+            // var movies = info.knownFor;
+            // var known = {};
+            // for (var i = 0; i < movies.length; i++) {
+            //     var id = movies[i].id;
+            //     var title = movies[i].title;
+            //     known[id] = title;
+            // }
+            // useData(name, known);
+            actor_card(info);
         });
 }
+function actor_card(info) {
+    var knownfor = info.knownFor;
+    console.log(knownfor);
+    document.querySelector('.actor-pic').src = info.image;
+    document.querySelector('#actor-name').textContent = info.name;
+    console.log(info.name + '-' + info.image);
 
-function useData(actor_movies) {
+    // document.querySelector('.movie-1').src = knownfor[0].image;
+    for (var i = 0; i < knownfor.length; i++) {
+        var movie = '.movie-' + (i + 1);
+        document.querySelector(movie).src = knownfor[i].image;
+        console.log(movie);
+    }
+    return;
+}
+
+function useData(name, actor_movies) {
     id_title = actor_movies;
-    var title_id = Object.keys(id_title)[0];
-    fetch(movie_imdbAPI + title_id).then(function (data) {
-        console.log(data.json());
-    });
+    // var actor_movies = {};
+    // var title_id = Object.keys(id_title)[0];
     console.log(id_title);
+    actor_card(id_title);
 }
 
 // const id_title = await getActor(actor);
 // console.log(id_title);
 // getGenre(genre);
-
